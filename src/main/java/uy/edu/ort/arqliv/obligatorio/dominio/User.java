7403f317e6ace7fa373bc76e7f1bdb513ce608dc
mauricio <karbunkle@gmail.com>
2014-05-10 16:22:07 -0300

    funciona store

--- src/main/java/uy/edu/ort/arqliv/obligatorio/dominio/User.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/dominio/User.java
@@ -3,10 +3,12 @@ package uy.edu.ort.arqliv.obligatorio.dominio;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 7360229243848231987L;
