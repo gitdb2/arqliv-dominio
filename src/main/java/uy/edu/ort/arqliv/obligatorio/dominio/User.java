5b3c3137cbc795a5fdf78a8fd3aa1d0ceba65b8f
mauricio <karbunkle@gmail.com>
2014-05-10 19:46:06 -0300

    tostring

--- src/main/java/uy/edu/ort/arqliv/obligatorio/dominio/User.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/dominio/User.java
@@ -25,4 +25,9 @@ public class User extends PersistentEntity implements Serializable {
 		this.name = name;
 	}
 
+	@Override
+	public String toString() {
+		return "User [name=" + name + "]";
+	}
+
 }
